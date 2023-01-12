a8896bfe6d9b9c4af71a729cab5ac906ad8c95e8
Nonso Andrew <nonsoandrew1@gmail.com>
2023-01-12 21:34:08 +0100

    serving the static files

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const tasks = require("./routes/task");
 const app = express();
 const port = 3000;
+const connect = require("./db/connect");
 
 //middlewares
-
+app.use(express.static("./public"))
 app.use(express.json());
 //routes
 app.use("/api/v1/tasks/", tasks);
+const start = async () => {
+	try {
+		await connect();
+		app.listen(port, () => {
+			console.log("App is listening at", port);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-app.listen(port, () => {
-	console.log("App is listening at", port);
-});
+start();
